c0d2fcb9819a8f15a171956881f20efb1e91f2fa
KyotaDev <luiszegarrafernandez@gmail.com>
2024-08-25 21:50:27 -0500

    Feat(contact): Add validator form

--- js/form.js
+++ js/form.js
@@ -11,32 +11,42 @@ function enableScroll() {
 }
 
 form.addEventListener('submit', function (event) {
-    event.preventDefault();
+    const name = document.getElementById('name').value.trim();
+    const subject = document.getElementById('subject').value.trim();
+    const message = document.getElementById('message').value.trim();
+    
+    if (!name || !subject || !message) {
+        event.preventDefault();
+        alert('Please fill out all required fields.');
+    }else{
+        event.preventDefault();
 
-    disableScroll();
-
-    loadingModal.classList.add('show');
-
-    fetch(form.action, {
-        method: form.method,
-        body: new FormData(form),
-    })
-    .then(response => response.ok)
-    .then(() => {
-        setTimeout(() => {
-            loadingModal.classList.remove('show');
-            successMessage.classList.add('show');
-
+        disableScroll();
+    
+        loadingModal.classList.add('show');
+    
+        fetch(form.action, {
+            method: form.method,
+            body: new FormData(form),
+        })
+        .then(response => response.ok)
+        .then(() => {
             setTimeout(() => {
-                successMessage.classList.remove('show');
-                enableScroll();
-                form.reset();
+                loadingModal.classList.remove('show');
+                successMessage.classList.add('show');
+    
+                setTimeout(() => {
+                    successMessage.classList.remove('show');
+                    enableScroll();
+                    form.reset();
+                }, 3000);
             }, 3000);
-        }, 3000);
-    })
-    .catch(error => {
-        loadingModal.classList.remove('show');
-        enableScroll();
-        alert('Something went wrong. Please try again later.');
-    });
+        })
+        .catch(error => {
+            loadingModal.classList.remove('show');
+            enableScroll();
+            alert('Something went wrong. Please try again later.');
+        });
+    }
 });
+
